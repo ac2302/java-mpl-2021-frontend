2af09ffa925c7298854fbd000d442bdb1f047431
Aryan Chavan <aryan@aryansdomain.com>
2021-12-11 23:06:42 +0530

    add styling to dashboard and move navbar to different component

--- src/components/DashboardPage.jsx
+++ src/components/DashboardPage.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 import config from "../config";
 import "./DashboardPage.css";
-import { random } from "nanoid";
+import Navbar from "./Navbar";
 
 function DashboardPage({ token }) {
 	const [user, setUser] = useState(null);
@@ -45,63 +45,7 @@ function DashboardPage({ token }) {
 
 	return (
 		<div className="dashboard">
-			<nav
-				className="navbar navbar-expand-md navbar-dark bg-dark"
-				aria-label="Fourth navbar example"
-			>
-				<div className="container-fluid">
-					<a className="navbar-brand" href="/">
-						Meeting Scheduler
-					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarsExample04"
-						aria-controls="navbarsExample04"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon"></span>
-					</button>
-
-					<div className="collapse navbar-collapse" id="navbarsExample04">
-						<ul className="navbar-nav me-auto mb-2 mb-md-0">
-							<li className="nav-item">
-								<a className="nav-link" href="/">
-									Home
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									New Event
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									New Daily Event
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									Schedule Meeting
-								</a>
-							</li>
-						</ul>
-						<button
-							type="button"
-							className="btn btn-danger"
-							onClick={() => {
-								localStorage.clear();
-								window.location = "/";
-							}}
-						>
-							Logout
-						</button>
-					</div>
-				</div>
-			</nav>
-
+			<Navbar />
 			{/* body */}
 
 			<div className="body">
@@ -155,7 +99,7 @@ function Task({ task }) {
 	return (
 		<div className="task">
 			<span>{task.name}</span>
-			<br />
+			<hr />
 			<span>
 				{start} to {end}
 			</span>
